6c339c15663c4e4c7b652f8740f2a4c4d46ddedb
Kevin Brown <kevinbrowntech@gmail.com>
2017-08-18 22:48:05 +1000

    WIP commit, doesn't work.

--- storybook/storybook.js
+++ storybook/storybook.js
@@ -1,19 +1,12 @@
-/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions, global-require, max-len */
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions, global-require */
 
 import { AppRegistry } from 'react-native';
 import { getStorybookUI, configure } from '@storybook/react-native';
-import 'babel-polyfill';
 
 // import stories
-const req = require.context('../src/components', true, /\.stories\.js$/);
-
-console.log(req);
-
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
+configure(() => {
+  require('./stories');
+}, module);
 
 // This assumes that storybook is running on the same host as your RN packager,
 // to set manually use, e.g. host: 'localhost' option
